79973609439d167b60fddc2d1c90468bb249f972
Helena Berens <helenaberens@helenas-mbp.dhcp.nd.edu>
2025-03-17 13:15:23 -0400

    navbar responsive design

--- feature4/src/Components/Shared/Navbar.jsx
+++ feature4/src/Components/Shared/Navbar.jsx
@@ -1,18 +1,33 @@
-import React from "react";
-import { Link } from "react-router-dom"; // We'll use React Router for navigation
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "../../styles.css"; // Import CSS file
 
-// nav bar for all pages- link to Home, Books Page, and Favorites
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // State to toggle menu visibility
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen); // Toggle the menu visibility
+  };
+
   return (
-    <nav className="navbar">
-      <ul className="nav-list">
+    <nav>
+      <div className="logo">Book App</div>
+
+      {/* Hamburger menu icon */}
+      <div className="hamburger-menu" onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      {/* Navigation links */}
+      <ul className={`nav-list ${isMenuOpen ? "active" : ""}`}>
         <li className="nav-item">
           <Link to="/" className="nav-link">
             Home
           </Link>
         </li>
-        <li className="books-page">
+        <li className="nav-item">
           <Link to="/books" className="nav-link">
             Books
           </Link>
